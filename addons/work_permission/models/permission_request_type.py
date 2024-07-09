a7fc4265f31a9b5dc8d794d901cf7517caee14c8
FilippoBordogna <bordognapippo99@gmail.com>
2024-07-09 18:04:16 +0200

    Aggiornamento Modello + View di PermissionRequestType (+ vademecum inutile)

--- addons/work_permission/models/permission_request_type.py
+++ addons/work_permission/models/permission_request_type.py
@@ -3,13 +3,14 @@ from odoo import api,fields, models
 class PermissionRequestType(models.Model):
     _name = "permission.request.type"
     _description = "Permission request type"
-    _order = "id desc"
+    _order = "name desc"
 
     name = fields.Char(required=True)
     description = fields.Text()
+    time_in = fields.Selection([ ('day', 'Day'), ('hour', 'Hour'), ('both', 'Day/Hour')])
+
 
     _sql_constraints = [
         ('check_name', 'unique(name)',
          'The name must be unique.')
     ]
-
