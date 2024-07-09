1db29b209a3a4f2236d6b3bbb939d4efaa2ed495
“acassina” <cassina99@gmail.com>
2024-07-09 16:52:47 +0200

    Setup nuovo modulo

---
+++ addons/work_permission/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Work permissions',   
+    'version': '1.0',
+    'category': 'MyAddons/WorkPermissions',
+    'summary': 'List of workers work permissions',
+    'description': "",
+    'depends': [
+        'base'
+    ],
+
+    'data': [
+        ],
+    'application': True,
+}
