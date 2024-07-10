243e9b376acc6c27587c16126addfeb1c9b3d099
FilippoBordogna <bordognapippo99@gmail.com>
2024-07-10 19:08:49 +0200

    Rimozione del campo time_in da modello e viste di PermissionRequestType

--- addons/work_permission/models/permission_request_type.py
+++ addons/work_permission/models/permission_request_type.py
@@ -2,15 +2,11 @@ from odoo import api,fields, models
 
 class PermissionRequestType(models.Model):
     _name = "permission.request.type"
-    _description = "Permission request type"
+    _description = "Permission Request Type"
     _order = "name desc"
+    _sql_constraints = [
+        ('check_name', 'unique(name)', 'The name must be unique.')
+    ]
 
     name = fields.Char(required=True)
     description = fields.Text()
-    time_in = fields.Selection([ ('day', 'Day'), ('hour', 'Hour'), ('both', 'Day/Hour')])
-
-
-    _sql_constraints = [
-        ('check_name', 'unique(name)',
-         'The name must be unique.')
-    ]
