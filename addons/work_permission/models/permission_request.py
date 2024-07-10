88242054db3b6e4bda44b5c24657e0b366b448cb
“acassina” <cassina99@gmail.com>
2024-07-10 23:26:13 +0200

    Implementata form view action di permission_request Inseriti pulsanti accept / refuse in visualizzazione tree view di permission_request Creati attributi e funzioni per la gestione della visualizzazione della parte time della datetime Scelta di date in creazione di permission_request dinamica
    PROBLEMA: Durante l'inserimento se tramite il widget cambi le date le date rimangono quelle di default

--- addons/work_permission/models/permission_request.py
+++ addons/work_permission/models/permission_request.py
@@ -25,14 +25,21 @@ class PermissionRequest(models.Model):
         default = 'new'
     )
     time_in = fields.Selection([ ('day', 'Day'), ('hour', 'Hour')], required = True, default = 'hour')
-    datetime_from = fields.Datetime(default = lambda self: datetime.now(), required = True) # Included
-    datetime_to = fields.Datetime(compute = "_compute_datetime_to", inverse = "_inverse_datetime_to") # Included
+    datetime_from = fields.Datetime(default = lambda self: datetime.now(), required = True, string="To") # Included
+    datetime_to = fields.Datetime(compute = "_compute_datetime_to", inverse = "_inverse_datetime_to", string="From") # Included
     duration = fields.Integer(required = True, default = 1)
+
+    date_from = fields.Date(string="From (date only)")
+    date_to = fields.Date(string="To (date only)")
+
+    formatted_datetime_from = fields.Char(compute='_compute_formatted_datetimes', store=True)
+    formatted_datetime_to = fields.Char(compute='_compute_formatted_datetimes', store=True)
     
     @api.depends('applicant', 'request_type_id', 'state', 'datetime_from', 'datetime_to')
     def _compute_title(self):
         for record in self:
-            record.title = "[" + record.state + "] " + record.applicant.name + ": " + record.request_type_id.name + " (" + (record.datetime_from + timedelta(hours=2)).strftime("%d/%m/%Y, %H:%M") + " - " + (record.datetime_to + timedelta(hours=2)).strftime("%d/%m/%Y, %H:%M") + ")"
+            #record.title = "[" + record.state + "] " + record.applicant.name + ": " + record.request_type_id.name + " (" + (record.datetime_from + timedelta(hours=2)).strftime("%d/%m/%Y, %H:%M") + " - " + (record.datetime_to + timedelta(hours=2)).strftime("%d/%m/%Y, %H:%M") + ")"
+            record.title = "[" + record.state + "] " + record.applicant.name + ": " + record.request_type_id.name + " (" + record.formatted_datetime_from + " - " + record.formatted_datetime_to + ")"
 
     @api.depends('datetime_from', 'duration')
     def _compute_datetime_to(self):
@@ -46,7 +53,7 @@ class PermissionRequest(models.Model):
     def _inverse_datetime_to(self):
         for record in self:
             if record.time_in == 'day':
-                record.duration = (record.datetime_to - record.datetime_from).days + 1
+                record.duration = ((record.datetime_to - record.datetime_from).days + 1) * 8
             else:
                 record.duration = (record.datetime_to - record.datetime_from).seconds//3600
 
@@ -58,4 +65,44 @@ class PermissionRequest(models.Model):
 
     @api.onchange('time_in')
     def _onchange_time_in(self):
-        self._compute_datetime_to()         
+        self._compute_datetime_to()     
+
+        
+    @api.depends('datetime_from', 'datetime_to', 'time_in')
+    def _compute_formatted_datetimes(self):
+        for record in self:
+            if record.time_in == 'hour':
+                # record.formatted_datetime_from = fields.Datetime.to_string(record.datetime_from)
+                # record.formatted_datetime_to = fields.Datetime.to_string(record.datetime_to)
+                record.formatted_datetime_from = record.datetime_from.strftime("%Y-%m-%d %H:%M")
+                record.formatted_datetime_to = record.datetime_to.strftime("%Y-%m-%d %H:%M")
+            else:
+                record.formatted_datetime_from = fields.Date.to_string(record.datetime_from.date())
+                record.formatted_datetime_to = fields.Date.to_string(record.datetime_to.date())
+
+    @api.onchange('time_in', 'datetime_from', 'datetime_to', 'date_from', 'date_to')
+    def _onchange_dates(self):
+        if self.time_in == 'hour':
+            if self.date_from:
+                self.datetime_from = fields.Datetime.to_datetime(self.date_from)
+            if self.date_to:
+                self.datetime_to = fields.Datetime.to_datetime(self.date_to)
+        else:
+            if self.datetime_from:
+                self.date_from = self.datetime_from.date()
+            if self.datetime_to:
+                self.date_to = self.datetime_to.date()
+
+    # Function for accept or refuse the permission request
+    def permission_request_action_accept(self):
+        for record in self:
+            if record.state != 'accepted' and record.state != 'refused':
+                record.state = 'accepted'
+        return True
+    
+
+    def permission_request_action_refuse(self):
+        for record in self:
+            if record.state != 'accepted' and record.state != 'refused':
+                record.state = 'refused'
+        return True
